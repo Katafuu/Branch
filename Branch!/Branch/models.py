d109e40519c74cf5555cad52922b37c00078457e
iramxx <ahmedmaghri4@gmail.com>
2025-01-23 01:56:16 +0100

    Fixed somestuff, Created the models for the data base of branch, the migrations doesn t wanna work I m going to look through it tomorrow

--- Branch!/Branch/models.py
+++ Branch!/Branch/models.py
@@ -1,57 +1,31 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Farmer Model
+# Create your models here.
+
+
 class Farmer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    # Region should be normalized as a separate model with a ForeignKey or ManyToManyField
-    region = models.CharField(max_length=30)  # You can replace this with a ForeignKey if needed
+    region = models.CharField(max_length=30)#I think we can make the region a many to many field, tho we ll need a data base
+#Tho we ll need a table with all possible regions
     latitude = models.DecimalField(
-        max_digits=9, decimal_places=6,
-        validators=[MinValueValidator(-90), MaxValueValidator(90)],
+        max_digits=9, decimal_places=6, 
+        validators=[MinValueValidator(-90), MaxValueValidator(90)], 
         null=True, blank=True
     )
     longitude = models.DecimalField(
-        max_digits=9, decimal_places=6,
-        validators=[MinValueValidator(-180), MaxValueValidator(180)],
+        max_digits=9, decimal_places=6, 
+        validators=[MinValueValidator(-180), MaxValueValidator(180)], 
         null=True, blank=True
     )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
-
-# Buyer Base Model
 class Buyer(models.Model):
     name = models.CharField(max_length=30)
     email = models.EmailField()
-
-    def __str__(self):
-        return self.name
-
-
-# Market Model
-class Market(Buyer):
-    siege_number = models.IntegerField()
-    market_name = models.CharField(max_length=100)  # Fix the field definition
-
-
-# Personal Buyer Model
-class Personal(Buyer):
-    age = models.PositiveIntegerField(
-        validators=[MinValueValidator(18), MaxValueValidator(99)]
-    )
-    latitude = models.DecimalField(
-        max_digits=9, decimal_places=6,
-        validators=[MinValueValidator(-90), MaxValueValidator(90)],
-        null=True, blank=True
-    )
-    longitude = models.DecimalField(
-        max_digits=9, decimal_places=6,
-        validators=[MinValueValidator(-180), MaxValueValidator(180)],
-        null=True, blank=True
-    )
     GENDER_CHOICES = [
         ('male', 'Male'),
         ('female', 'Female'),
@@ -60,4 +34,22 @@ class Personal(Buyer):
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
 
     def __str__(self):
-        return f"{self.name} ({self.gender})"
+        return f"{self.name}"
+
+class Market(Buyer):
+    siege_number = models.IntegerField()
+    market_name = models.CharField
+
+class Personal(Buyer):
+    age = models.PositiveIntegerField(MinValueValidator(18), MaxValueValidator(99))
+    gender = models.CharField(max_length=50)
+    latitude = models.DecimalField(
+        max_digits=9, decimal_places=6, 
+        validators=[MinValueValidator(-90), MaxValueValidator(90)], 
+        null=True, blank=True
+    )
+    longitude = models.DecimalField(
+        max_digits=9, decimal_places=6, 
+        validators=[MinValueValidator(-180), MaxValueValidator(180)], 
+        null=True, blank=True
+    )
