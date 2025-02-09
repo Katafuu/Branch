d6a957d6ed4d1f9d74b396c5891c86fbc25cd780
Aly Assaf <galaxyfriend21@gmail.com>
2025-02-09 13:12:41 +0100

    pushing to production

--- Branch/settings.py
+++ Branch/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9_jk2+5!9+*rjl&io9l3l3i$br9&rclh#c@8cxysjn%%=uo22f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
